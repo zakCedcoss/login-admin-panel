78344d7d61454ceeb9dd24ffbbf96bd107addf0f
Zak <zeeshan@cedcommerce.com>
2022-09-29 10:52:43 +0530

    error handling corrected

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -12,11 +12,9 @@ import {
 import { useLocation, useNavigate } from "react-router-dom";
 
 function Login() {
-  const TOKEN =
-    "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJ1c2VyX2lkIjoiMSIsInJvbGUiOiJhcHAiLCJpYXQiOjE1MzkwNTk5NzgsImlzcyI6Imh0dHBzOlwvXC9hcHBzLmNlZGNvbW1lcmNlLmNvbSIsImF1ZCI6ImV4YW1wbGUuY29tIiwibmJmIjoxNTM5MDU5OTc4LCJ0b2tlbl9pZCI6MTUzOTA1OTk3OH0.GRSNBwvFrYe4H7FBkDISVee27fNfd1LiocugSntzxAUq_PIioj4-fDnuKYh-WHsTdIFMHIbtyt-uNI1uStVPJQ4K2oYrR_OmVe5_zW4fetHyFmoOuoulR1htZlX8pDXHeybRMYlkk95nKZZAYQDB0Lpq8gxnTCOSITTDES0Jbs9MENwZWVLfyZk6vkMhMoIAtETDXdElIdWjP6W_Q1kdzhwqatnUyzOBTdjd_pt9ZkbHHYnv6gUWiQV1bifWpMO5BYsSGR-MW3VzLqsH4QetZ-DC_AuF4W2FvdjMRpHrsCgqlDL4I4ZgHJVp-iXGfpug3sJKx_2AJ_2aT1k5sQYOMA";
-
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [message, setMessage] = useState("");
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [error, setError] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
@@ -32,6 +30,7 @@ function Login() {
       setIsLoading(false);
       setIsLoggedIn(false);
       setError(true);
+      setMessage("Fields should not be empty");
       return;
     }
     setIsLoading(true);
@@ -59,12 +58,14 @@ function Login() {
           setIsLoggedIn(false);
           setError(true);
         }
+        setMessage(data.message);
       })
       .catch((error) => {
         console.log(error);
         setIsLoading(false);
         setIsLoggedIn(false);
         setError(true);
+        setMessage(error);
       });
   };
 
@@ -101,7 +102,7 @@ function Login() {
         <div className="toast">
           <Frame>
             <Toast
-              content="Error Occurred"
+              content={message}
               duration={3000}
               error
               onDismiss={() => setError(false)}
